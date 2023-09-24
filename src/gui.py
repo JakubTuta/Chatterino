cdaff6306d9c8a1542fb6e6c0158ea458863eabb
Jakub Tutka <jakubtutka02@gmail.com>
2023-09-24 22:43:23 +0200

    Choose server and input password

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,9 @@
 from google.cloud import firestore
-import tkinter as tk
 import customtkinter as ctk
+from firebase.serverStore import fetchServers
+
+
+selectedServer = None
 
 
 class ScrollableFrame(ctk.CTkScrollableFrame):
@@ -11,38 +14,64 @@ class ScrollableFrame(ctk.CTkScrollableFrame):
         font = ctk.CTkFont(family="Arial", size=25)
 
         for i, server in enumerate(servers):
-            ctk.CTkButton(self, text=server, font=font, width=kwargs["width"], height=50) \
-                .grid(row=i, column=0, pady=10, padx=5)
+            ctk.CTkButton(
+                self,
+                command=lambda: ScrollableFrame.__handleButtonClick(server),
+                text=server["name"],
+                font=font,
+                width=kwargs["width"],
+                height=50
+            ).pack(pady=10, padx=50)
+
+    @staticmethod
+    def __handleButtonClick(server):
+        global selectedServer
+        selectedServer = server
+        GuiApp.passwordDialog()
+
+
+class Window(ctk.CTk):
+    def __init__(self, **kwargs):
+        width = kwargs.pop("width", 0)
+        height = kwargs.pop("height", 0)
+        super().__init__()
+
+        self.geometry(f"{width}x{height}")
+        self.resizable(False, False)
 
 
 class GuiApp:
-    windowWidth, windowHeight = 700, 700
+    windowWidth, windowHeight = 600, 700
 
     @staticmethod
     def run(side: str, userData: dict, userRef: firestore.DocumentReference):
-        root = tk.Tk()
-        root.geometry(f"{GuiApp.windowWidth}x{GuiApp.windowHeight}")
-        root.resizable(False, False)
+        allServers = fetchServers()
+        activeServers = [server for server in allServers if server["isActive"]]
 
-        GuiApp.createServerSelectionView(root)
+        if side == "host":
+            pass
 
-        root.mainloop()
+        elif side == "join":
+            GuiApp.createWindow(activeServers)
 
     @staticmethod
-    def createServerSelectionView(root):
-        def enterPassword():
-            ctk.CTkLabel(root, text="Enter password", font=font).grid(row=2, column=0, pady=10)
+    def createWindow(servers):
+        app = Window(width=GuiApp.windowWidth, height=GuiApp.windowHeight)
 
-            entry = ctk.CTkEntry(root)
-            entry.grid(row=3, column=0, pady=10)
+        GuiApp.createServerSelectionView(app, servers)
 
-        font = ctk.CTkFont(family="Arial", size=25)
+        app.mainloop()
 
+    @staticmethod
+    def createServerSelectionView(app, servers):
         ScrollableFrame(
-            master=root,
+            master=app,
             width=GuiApp.windowWidth * .9,
             height=GuiApp.windowHeight * .75,
-            servers=["jeden", "dwa"]
+            servers=servers
         ).pack()
 
-        ctk.CTkButton(root, text="Join!", font=font).pack(pady=10)
+    @staticmethod
+    def passwordDialog():
+        while ctk.CTkInputDialog(text="Enter password", title="Password").get_input() != selectedServer["password"]:
+            pass
